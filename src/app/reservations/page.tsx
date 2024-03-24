07eebe4bd44b665a6ad4495e817ab2182890d8ff
NewBieCoderXD <fruity5555@gmail.com>
2024-03-24 19:12:20 +0700

    add white background to reservations/page

--- src/app/reservations/page.tsx
+++ src/app/reservations/page.tsx
@@ -32,35 +32,37 @@ export default async function(){
     }
 
     return (
-        <div className="flex items-center justify-center">
-            {
-            reservations.map((reservation,index)=>{
+        <div className="flex items-center justify-center text-black m-2">
+            <div className="flex items-center justify-center bg-white rounded-2xl gap-2">
+                {
+                reservations.map((reservation,index)=>{
 
-                return (
-                    <div key={index} className="flex w-3/4 h-20 items-center justify-center gap-3">
-                        <Link 
-                            href={getRestaurantUrl(reservation.restaurantId)}
-                            className="h-full aspect-square"
-                        >
-                            <RestaurantImage
-                                alt={reservation.restaurant?.name||""}
-                                src={getRestaurantImageSrc(reservation.restaurantId)}
-                                width={10}
-                                height={10}
-                                sizes="50vw"
-                                // layout={'fill'}
-                                className="w-full h-full object-cover rounded-2xl"
-                            />
-                        </Link>
-                        <div className="">
-                            <Typography>Restaurant: {reservation.restaurant?.name}</Typography>
-                            <Typography>Reservation Date: {new Date(reservation.reservationDate).toLocaleDateString("en-UK")}</Typography>
-                            <Typography>Reservation Time: {reservation.reservationPeriod.startTime}-{reservation.reservationPeriod.endTime}</Typography>
+                    return (
+                        <div key={index} className="flex h-20 items-center justify-center gap-3 p-2">
+                            <Link 
+                                href={getRestaurantUrl(reservation.restaurantId)}
+                                className="h-full aspect-square"
+                            >
+                                <RestaurantImage
+                                    alt={reservation.restaurant?.name||""}
+                                    src={getRestaurantImageSrc(reservation.restaurantId)}
+                                    width={10}
+                                    height={10}
+                                    sizes="50vw"
+                                    // layout={'fill'}
+                                    className="w-full h-full object-cover rounded-2xl"
+                                />
+                            </Link>
+                            <div className="">
+                                <Typography>Restaurant: {reservation.restaurant?.name}</Typography>
+                                <Typography>Reservation Date: {new Date(reservation.reservationDate).toLocaleDateString("en-UK")}</Typography>
+                                <Typography>Reservation Time: {reservation.reservationPeriod.startTime}-{reservation.reservationPeriod.endTime}</Typography>
+                            </div>
                         </div>
-                    </div>
-                )
-            })
-            }
+                    )
+                })
+                }
+            </div>
         </div>
     )
 }
