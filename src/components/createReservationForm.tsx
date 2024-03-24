f569fe47d2fe7dc3ce1ecb929b1db7108fdedb35
NewBieCoderXD <fruity5555@gmail.com>
2024-03-24 22:05:57 +0700

    add date picker

--- src/components/createReservationForm.tsx
+++ src/components/createReservationForm.tsx
@@ -2,6 +2,10 @@
 import { TextField, Button, Autocomplete } from "@mui/material";
 import { Formik } from "formik";
 import { SyntheticEvent, useState } from "react";
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs, { Dayjs } from 'dayjs';
 
 export default function({
     token
@@ -27,31 +31,30 @@ export default function({
     return (
         <div className="h-full flex items-center justify-center m-2">
             <Formik
-            initialValues={{
-                restaurantName: "",
-                reservationDate: "",
-                reservationPeriod: {
-                    startTime:"",
-                    endTime:""
-                }
-            }}
-            onSubmit={async (values,{setSubmitting, setErrors})=>{
-                const result = await fetch("/api/v1/reservations",{
-                    method:"POST",
-                    headers:{
-                        Authorization: `Bearer ${token}`
-                    },
-                    body: JSON.stringify(values)
-                })
-                console.log(await result.text())
-                if(!result.ok){
-                    setErrors({
-                        restaurantName:"Wrong restaurant name or unavailable period"
+                initialValues={{
+                    restaurantName: "",
+                    reservationDate: null as (Dayjs|null),
+                    reservationPeriod: {
+                        startTime:"",
+                        endTime:""
+                    }
+                }}
+                onSubmit={async (values,{setSubmitting, setErrors})=>{
+                    const result = await fetch("/api/v1/reservations",{
+                        method:"POST",
+                        headers:{
+                            Authorization: `Bearer ${token}`
+                        },
+                        body: JSON.stringify(values)
                     })
-                }
-                return;
-            }}
-            
+                    console.log(await result.text())
+                    if(!result.ok){
+                        setErrors({
+                            restaurantName:"Wrong restaurant name or unavailable period"
+                        })
+                    }
+                    return;
+                }}
             >
             {({
                 values,
@@ -62,40 +65,45 @@ export default function({
                 handleSubmit,    
                 isSubmitting,    
             }) => (
-                    <form  onSubmit={handleSubmit} className="flex flex-col gap-2 w-2/3 sm:w-1/2 bg-white rounded-2xl p-2">
-                        <Autocomplete
-                            disablePortal
-                            sx={{
-                                width:"75%",
-                                alignSelf:"center"
+                <form  onSubmit={handleSubmit} className="flex flex-col gap-2 w-2/3 sm:w-1/2 bg-white rounded-2xl p-2">
+                    <Autocomplete
+                        disablePortal
+                        sx={{
+                            width:"100%",
+                            alignSelf:"center"
+                        }}
+                        options={restaurantsList}
+                        filterOptions={(options, state) => options}
+                        // sx={{ width: 300 }}
+                        renderInput={(params) => <TextField 
+                            {...params} 
+                            label="Restaurant Name" 
+                            InputProps={{
+                                ...params.InputProps,
+                                // type: 'search',
                             }}
-                            options={restaurantsList}
-                            filterOptions={(options, state) => options}
-                            // sx={{ width: 300 }}
-                            renderInput={(params) => <TextField 
-                                {...params} 
-                                label="Restaurant Name" 
-                                InputProps={{
-                                    ...params.InputProps,
-                                    // type: 'search',
-                                }}
-                            />}
-                            value={values.restaurantName}
-                            onInputChange={onRestaurantNameChange}
+                        />}
+                        value={values.restaurantName}
+                        onInputChange={onRestaurantNameChange}
+                        onChange={handleChange}
+                        freeSolo
+                        autoSelect
+                    />
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <DatePicker
+                            label="reservation date"
+                            value={values.reservationDate}
                             onChange={handleChange}
-                            freeSolo
-                            autoSelect
-                        />
-
-                        <Button 
-                            type="submit"
-                            disabled={isSubmitting}
-                        >
-                            reserve now!
-                        </Button>
-                    </form>
-                )
-                }
+                        ></DatePicker>
+                    </LocalizationProvider>
+                    <Button 
+                        type="submit"
+                        disabled={isSubmitting}
+                    >
+                        reserve now!
+                    </Button>
+                </form>
+            )}
             </Formik>
         </div>
     )
